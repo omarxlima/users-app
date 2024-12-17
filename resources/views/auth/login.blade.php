cac69e02f31b5d324ca46f8f04aa01d9420d684f
omarxlima <marxmaciel@gmail.com>
2024-12-17 09:31:15 -0300

    tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,45 @@
-login
+@extends('layouts.auth')
+
+@section('body-class', 'login-page')
+
+@section('content')
+
+
+    <div class="login-box">
+        <div class="login-logo"> <a href="../index2.html"><b>Admin</b>LTE</a> </div> <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
+                <form action="{{route('login')}}" method="post">
+                    @csrf
+                    <div class="input-group mb-3"> 
+                        <div class="input-group-text"> <span class="bi bi-envelope"></span> </div>
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}">
+                        @error('email')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="input-group mb-3"> 
+                        <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" value="{{ old('password') }}">
+                        @error('password')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div> <!--begin::Row-->
+                    <div class="row">
+                            <div class="d-grid gap-2"> <button type="submit" class="btn btn-primary">Sign In</button> </div>
+                            
+                      
+                    </div> <!--end::Row-->
+                </form>
+                
+                <p class="mb-1"> <a href="#">I forgot my password</a> </p>
+                <p class="mb-0"> <a href="{{ route('register') }}" class="text-center">
+                        Register a new membership
+                    </a> </p>
+            </div> <!-- /.login-card-body -->
+        </div>
+    </div> <!-- /.login-box -->
+
+    
+@endsection
